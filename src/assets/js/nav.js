1ced1ecd6677d6495925b554455865ba3bf0a0ec
Silver Alcid <148133773+silveralcid@users.noreply.github.com>
2023-12-30 14:36:00 -0500

    Mobile Nav Menu closes after click.

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,46 +1,53 @@
-// add classes for mobile navigation toggling
+// Existing code for mobile navigation toggling
 var CSbody = document.querySelector("body");
 const CSnavbarMenu = document.querySelector("#cs-navigation");
 const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
 
-CShamburgerMenu.addEventListener('click', function() {
-	CShamburgerMenu.classList.toggle("cs-active");
-	CSnavbarMenu.classList.toggle("cs-active");
-	CSbody.classList.toggle("cs-open");
-	// run the function to check the aria-expanded value
-	ariaExpanded();
-});
+CShamburgerMenu.addEventListener('click', toggleMenu);
 
-// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
-function ariaExpanded() {
-	const csUL = document.querySelector('#cs-expanded');
-	const csExpanded = csUL.getAttribute('aria-expanded');
-
-	if (csExpanded === 'false') {
-		csUL.setAttribute('aria-expanded', 'true');
-	} else {
-		csUL.setAttribute('aria-expanded', 'false');
-	}
+function toggleMenu() {
+    CShamburgerMenu.classList.toggle("cs-active");
+    CSnavbarMenu.classList.toggle("cs-active");
+    CSbody.classList.toggle("cs-open");
+    ariaExpanded();
 }
 
-    // This script adds a class to the body after scrolling 100px
-    // and we used these body.scroll styles to create some on scroll 
-    // animations with the navbar
-    
-    document.addEventListener('scroll', (e) => { 
-        const scroll = document.documentElement.scrollTop;
-        if(scroll >= 100){
-    document.querySelector('body').classList.add('scroll')
-        } else {
-        document.querySelector('body').classList.remove('scroll')
+function ariaExpanded() {
+    const csUL = document.querySelector('#cs-expanded');
+    const csExpanded = csUL.getAttribute('aria-expanded');
+
+    if (csExpanded === 'false') {
+        csUL.setAttribute('aria-expanded', 'true');
+    } else {
+        csUL.setAttribute('aria-expanded', 'false');
+    }
+}
+
+// Code to close the mobile menu when a link is clicked
+const navLinks = document.querySelectorAll('#cs-navigation a');
+
+navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        // Check if menu is open before toggling
+        if (CSbody.classList.contains('cs-open')) {
+            toggleMenu();
         }
     });
-	
-    // mobile nav toggle code
-    const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
-        for (const item of dropDowns) {
-            const onClick = () => {
-            item.classList.toggle('cs-active')
-        }
-        item.addEventListener('click', onClick)
-        }
+});
+
+// Existing code for scroll event and dropdown toggle
+document.addEventListener('scroll', (e) => { 
+    const scroll = document.documentElement.scrollTop;
+    if (scroll >= 100) {
+        document.querySelector('body').classList.add('scroll');
+    } else {
+        document.querySelector('body').classList.remove('scroll');
+    }
+});
+
+const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+for (const item of dropDowns) {
+    item.addEventListener('click', () => {
+        item.classList.toggle('cs-active');
+    });
+}
